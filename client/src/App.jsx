71af29fe383a08742b1ff158b8426b7a01d63b2b
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-08-27 11:52:11 +0100

    Add routing and file structure for client-side pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import HomePage from './pages/HomePage.js';
+import SignInPage from './pages/SignInPage.js';
+import SignUpPage from './pages/SignUpPage.js';
+import SignOutPage from './pages/SignOutPage.js';
+import DashboardPage from './pages/DashboardPage.js';
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function Router() {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<HomePage/>} />
+                <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/sign-in" element={<SignInPage />} />
+                <Route path="/sign-up" element={<SignUpPage />} />
+                <Route path="/sign-out" element={<SignOutPage />}/>
+            </Routes>
+        </BrowserRouter>
+    )
 }
 
-export default App
+export default Router;
