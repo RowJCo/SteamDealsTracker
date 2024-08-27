a25de749520efaef1aa4d7c23df084bac58de811
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-08-27 11:51:52 +0100

    Further configure tailwindcss

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./server/build/index.html", "./src/components/*.{js,jsx}", "./src/App.jsx", "./src/pages/*.{js,jsx}"],
+  content: ["./index.html", "./src/components/*.{js,jsx}", "./src/App.jsx", "./src/pages/*.{js,jsx}"],
   theme: {
     extend: {},
   },
