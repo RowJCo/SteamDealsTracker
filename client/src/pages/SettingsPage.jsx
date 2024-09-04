bbbfa7520f61ce16dd8f06ffac27166a6b9cf774
Rowen Collier <163330547+RowJCo@users.noreply.github.com>
2024-09-04 11:55:59 +0100

    Update SettingsPage.jsx

--- client/src/pages/SettingsPage.jsx
+++ client/src/pages/SettingsPage.jsx
@@ -1,1 +1,19 @@
+import Header from "../components/Header.jsx";
+import Footer from "../components/Footer.jsx";
+import RequireAuth from "../components/RequireAuth.jsx";
 
+const SettingsPage = () => {
+  return(
+    <div>
+      <Header />
+      <RequireAuth>
+        <div>
+          
+        </div>
+      </RequireAuth>
+      <Footer />
+    </div>
+  )
+}
+
+export default SettingsPage;
