acaa29f73fce154b05fcdb9092a99ca317cc049d
Rowen Collier <163330547+RowJCo@users.noreply.github.com>
2024-09-04 14:02:54 +0100

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import Home from "../components/Home.jsx";
 
-function HomePage() {
+const HomePage = () => {
     return (
         <div>
             <Header />
