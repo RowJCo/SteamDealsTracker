3cca8ea69a6079e9f854da138ae3641631cba011
Rowen Collier <163330547+RowJCo@users.noreply.github.com>
2024-09-04 14:02:18 +0100

    Update SettingsPage.jsx

--- client/src/pages/SettingsPage.jsx
+++ client/src/pages/SettingsPage.jsx
@@ -1,19 +1,39 @@
 import Header from "../components/Header.jsx";
 import Footer from "../components/Footer.jsx";
 import RequireAuth from "../components/RequireAuth.jsx";
+import gameStore from '../stores/gameStore.js';
+import userStore from '../stores/userStore.js';
 
 const SettingsPage = () => {
-  return(
+
+  const handleDeleteAccount = async () => {
+    try {
+        await gameStore.deleteUsersUserGames();
+        await userStore.deleteUser();
+    } catch (error) {
+        console.error("Error deleting user account and games:", error);
+    }
+  };
+
+  return (
     <div>
       <Header />
       <RequireAuth>
-        <div>
-          
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+          <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+            <h2 className="text-2xl font-bold text-gray-800 mb-6 text-center">Account Settings</h2>
+            <button 
+              className="bg-red-500 text-white px-4 py-2 rounded-lg shadow-sm hover:bg-red-600"
+              onClick={handleDeleteAccount}
+            >
+              Delete Account
+            </button>
+          </div>
         </div>
       </RequireAuth>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
 export default SettingsPage;
