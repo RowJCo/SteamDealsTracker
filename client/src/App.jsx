9310865ea0690bb3370296037e449256bc065612
Rowen Collier <163330547+RowJCo@users.noreply.github.com>
2024-09-04 11:42:35 +0100

    Add settings page to routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage.jsx';
+import SettingsPage from './pages/SettingsPage.jsx';
 import SignInPage from './pages/SignInPage.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
 import SignOutPage from './pages/SignOutPage.jsx';
@@ -12,6 +13,7 @@ function App() {
             <Routes>
                 <Route index element={<HomePage/>} />
                 <Route path="/dashboard" element={<DashboardPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
                 <Route path="/sign-in" element={<SignInPage />} />
                 <Route path="/sign-up" element={<SignUpPage />} />
                 <Route path="/sign-out" element={<SignOutPage />}/>
