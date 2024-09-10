632332d960b16bed8120902cca190ba9483a2c19
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-09-10 14:09:26 +0100

    Add check for duplicate email

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,6 +7,13 @@ import pool from "../config/db.js";
 const signUp = async (req, res) => {
     try {
         const { email, password } = req.body;
+        //checks if the email is already in use
+        const emailQuery = "SELECT * FROM users WHERE email = $1";
+        const emailValues = [email];
+        const emailResult = await pool.query(emailQuery, emailValues);
+        if (emailResult.rows.length > 0) {
+            return res.status(400).json({ message: "Email already in use" });
+        }
         const hashedPassword = await bcrypt.hash(password, 10);
         const query = "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *";
         const values = [email, hashedPassword];
