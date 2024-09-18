b2aa86af2fa345e25d557d5fa92ad7a2679849db
Rowen Collier <163330547+RowJCo@users.noreply.github.com>
2024-09-18 15:41:34 +0100

    Update server.js to increase request limit

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ const __dirname = dirname(__filename);
 
 var limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // limit each IP to 100 requests per windowMs
+	limit: 1000, // limit each IP to 1000 requests per windowMs
 	skipFailedRequests : true
 })
 
