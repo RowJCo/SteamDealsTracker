6d221fc9ed0743f06871d96166fad4b7069cf6dd
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-10-06 17:43:33 +0100

    Refactor SignUpForm component to display error messages for email and password fields

--- components/signUpForm.jsx
+++ components/signUpForm.jsx
@@ -17,6 +17,24 @@ export default function SignUpForm() {
           placeholder="Email"
           className="input input-bordered w-full max-w-xs"
         />
+        {formState.errors?.email && (
+          <div role="alert" className="alert alert-warning">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 shrink-0 stroke-current"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <span>{formState.errors?.email}</span>
+          </div>
+        )}
       </div>
       <div className="mb-3">
         <input
@@ -26,6 +44,24 @@ export default function SignUpForm() {
           placeholder="Password"
           className="input input-bordered w-full max-w-xs"
         />
+        {formState.errors?.password && (
+          <div role="alert" className="alert alert-warning">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 shrink-0 stroke-current"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              />
+            </svg>
+            <span>{formState.errors?.password}</span>
+          </div>
+        )}
       </div>
       <div className="mb-3">
         <button className="btn btn-primary">Create Account</button>
