b4494dd6c6048283f80ccce9dcbfe72da295efab
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-10-07 16:36:04 +0100

    Refactor SignUpForm to remove success message and change to redirect

--- components/signUpForm.jsx
+++ components/signUpForm.jsx
@@ -68,26 +68,8 @@ export default function SignUpForm() {
           )}
         </div>
         <div className="mb-3 flex justify-center">
-          <button className="btn btn-primary">Create Account</button>
+          <button className="btn btn-primary">Sign Up</button>
         </div>
-        {formState.errors?.success && (
-          <div role="alert" className="alert alert-success">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-6 w-6 shrink-0 stroke-current"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
-            <span>{formState.errors?.success}</span>
-          </div>
-        )}
       </form>
     </>
   );
