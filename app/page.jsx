4d2e9c7898a8fd852ca02426106ebb60b1898478
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-10-07 15:49:06 +0100

    Refactor page component to conditionally render content based on user login status

--- app/page.jsx
+++ app/page.jsx
@@ -1,126 +1,138 @@
 import React from "react";
 import Link from "next/link";
+import { getCookieData } from "../lib/getCookieData";
 
-export default function page() {
+export default async function page() {
+  //get the user data from the cookie
+  const user = await getCookieData();
   return (
     <>
-      <h1 className="text-4xl text-center py-2 font-black">
-        Is there a game on steam that you want that never seems to be at a price
-        you can afford?
-      </h1>
-      <h2 className="text-2xl text-center py-2 ">
-        With Steam Deals Tracker it's simple!
-      </h2>
-      <ul class="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical py-6">
-        <li>
-          <div class="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-5 w-5"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-          <div class="timeline-start mb-10 md:text-end">
-            <time class="font-mono italic">Step 1:</time>
-            <div class="text-lg font-black">Create an account</div>
-            The first step is to create an account with us using the email you
-            would like to receive notifications to. This will include an email
-            verification step to ensure that you are the owner of the email
-            address.
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div class="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-5 w-5"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-          <div class="timeline-end mb-10">
-            <time class="font-mono italic">Step 2:</time>
-            <div class="text-lg font-black">Find the game you want</div>
-            Look through the catologue of steam games and find the game you
-            want. Once you find it, click add to wishlist.
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div class="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-5 w-5"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-          <div class="timeline-start mb-10 md:text-end">
-            <time class="font-mono italic">Step 3:</time>
-            <div class="text-lg font-black">Pick a price</div>
-            Once you have added the game to your wishlist, you can set a price
-            at which you would like to be notified when the game reaches that
-            price.
-          </div>
-          <hr />
-        </li>
-        <li>
-          <hr />
-          <div class="timeline-middle">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              class="h-5 w-5"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                clip-rule="evenodd"
-              />
-            </svg>
-          </div>
-          <div class="timeline-end mb-10">
-            <time class="font-mono italic">Step 4:</time>
-            <div class="text-lg font-black">
-              Wait for the email notification
-            </div>
-            Once you have set the price, you can sit back and relax. When the
-            game reaches the price you set, you will receive an email
-            notification with a link to buy the game on steam.
-          </div>
-          <hr />
-        </li>
-      </ul>
-      <p className="text-2xl text-center py-8">
-        {" "}
-        Don't have an account?{" "}
-        <Link href="/signup">
-          <strong>Create one</strong>
-        </Link>
-      </p>
+      {user && (
+        <>
+          <p>Welcome you are logged in!</p>
+        </>
+      )}
+      {!user && (
+        <>
+          <h1 className="text-4xl text-center py-2 font-black">
+            Is there a game on steam that you want that never seems to be at a
+            price you can afford?
+          </h1>
+          <h2 className="text-2xl text-center py-2 ">
+            With Steam Deals Tracker it's simple!
+          </h2>
+          <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical py-6">
+            <li>
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-start mb-10 md:text-end">
+                <time className="font-mono italic">Step 1:</time>
+                <div className="text-lg font-black">Create an account</div>
+                The first step is to create an account with us using the email
+                you would like to receive notifications to. This will include an
+                email verification step to ensure that you are the owner of the
+                email address.
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end mb-10">
+                <time className="font-mono italic">Step 2:</time>
+                <div className="text-lg font-black">Find the game you want</div>
+                Look through the catologue of steam games and find the game you
+                want. Once you find it, click add to wishlist.
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-start mb-10 md:text-end">
+                <time className="font-mono italic">Step 3:</time>
+                <div className="text-lg font-black">Pick a price</div>
+                Once you have added the game to your wishlist, you can set a
+                price at which you would like to be notified when the game
+                reaches that price.
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end mb-10">
+                <time className="font-mono italic">Step 4:</time>
+                <div className="text-lg font-black">
+                  Wait for the email notification
+                </div>
+                Once you have set the price, you can sit back and relax. When
+                the game reaches the price you set, you will receive an email
+                notification with a link to buy the game on steam.
+              </div>
+              <hr />
+            </li>
+          </ul>
+          <p className="text-2xl text-center py-8">
+            {" "}
+            What are you waiting for?{" "}
+            <Link href="/signup">
+              <strong>Create an account now</strong>
+            </Link>
+          </p>
+        </>
+      )}
     </>
   );
 }
