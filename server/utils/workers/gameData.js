0232b54a3f9fdb86bd50856c3bbb913ff8d1e166
RowJCo <163330547+RowJCo@users.noreply.github.com>
2024-10-21 21:09:41 +0100

    Add delay to gameData.js to not overload hosting service

--- server/utils/workers/gameData.js
+++ server/utils/workers/gameData.js
@@ -3,6 +3,9 @@
 //import dependencies
 import { connectEditDb, closeDb, runQueryWithRetry } from "../db.js";
 
+// Delay function to wait for a specified number of milliseconds
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 const gameData = async () => {
   try {
     //connect to the database
@@ -16,25 +19,31 @@ const gameData = async () => {
     const games = data.applist.apps;
     //create variable to keep track of the number of games inserted
     let gamesInserted = 0;
-    //insert the game data into the database
-    games.forEach(async (game) => {
-      if (
-        game.name !== null ||
-        game.name !== "" ||
-        game.name !== undefined ||
-        game.name !== " "
-      ) {
-        await runQueryWithRetry(
-          db,
-          "INSERT INTO games (id, name) VALUES ($1, $2) ON CONFLICT (id) DO NOTHING",
-          [game.appid, game.name]
-        );
-        gamesInserted++;
-        if (gamesInserted % 100 === 0) {
-          console.log("Inserted " + gamesInserted + " games");
+    //insert the game data into the database in batches of 10,000
+    for (let i = 0; i < games.length; i += 10000) {
+      const batch = games.slice(i, i + 10000);
+      for (const game of batch) {
+        if (
+          game.name !== null &&
+          game.name !== "" &&
+          game.name !== undefined &&
+          game.name !== " "
+        ) {
+          await runQueryWithRetry(
+            db,
+            "INSERT INTO games (id, name) VALUES ($1, $2) ON CONFLICT (id) DO NOTHING",
+            [game.appid, game.name]
+          );
+          gamesInserted++;
+          if (gamesInserted % 100 === 0) {
+            console.log("Inserted " + gamesInserted + " games");
+          }
         }
       }
-    });
+      console.log(`Processed batch ${i / 10000 + 1}`);
+      // Wait for 1 minute before processing the next batch
+      await delay(60000);
+    }
     //close the database connection
     closeDb(db);
     console.log("Game data fetched");
